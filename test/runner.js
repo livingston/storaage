6cd0f69d5fdeff48a14412d57f7f59083b05620b
Livingston Samuel <livingston.samuel@gmail.com>
2014-04-16 01:11:23 +0530

    configured Travis & SauceLabs for CI

---
+++ test/runner.js
@@ -0,0 +1,29 @@
+var MochaSauce = require("../node_modules/mocha-sauce/index.js");
+
+// configure
+var sauce = new MochaSauce({
+    name: "SessionStore", // your project name
+
+    // the test url
+    url: "http://localhost/test" // point to the site running your mocha tests
+});
+
+
+// setup what browsers to test with
+sauce.browser({ browserName: "chrome", platform: "Windows 7" });
+sauce.browser({ browserName: "firefox", platform: "Windows 7" });
+
+
+sauce.on('init', function(browser) {
+  console.log('  init : %s %s', browser.browserName, browser.platform);
+});
+
+sauce.on('start', function(browser) {
+  console.log('  start : %s %s', browser.browserName, browser.platform);
+});
+
+sauce.on('end', function(browser, res) {
+  console.log('  end : %s %s : %d failures', browser.browserName, browser.platform, res.failures);
+});
+
+sauce.start();
