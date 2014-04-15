a36838d13f4182b65267b8d3fbac3576f6dc0779
Livingston Samuel <livingston.samuel@gmail.com>
2014-04-15 23:47:41 +0530

    Added SessionStore.js

---
+++ SessionStore.js
@@ -0,0 +1,34 @@
+(function (global) {
+    'use strict';
+    var SessionStore = function (namespace, data) {
+        this.ns = namespace;
+
+        this.data = data;
+    };
+
+    Object.defineProperties(SessionStore.prototype, {
+        data: {
+            get: function () {
+                return JSON.parse(window.sessionStorage.getItem(this.ns));
+            },
+            set: function (data) {
+                window.sessionStorage.setItem(this.ns, JSON.stringify(data || {}));
+            }
+        },
+        getItem: {
+            value: function (key) {
+                return this.data[key];
+            }
+        },
+        setItem: {
+            value: function (key, value) {
+                var data = this.data;
+                data[key] = value;
+
+                this.data = data;
+            }
+        }
+    });
+
+    global.SessionStore = SessionStore;
+}(window));
