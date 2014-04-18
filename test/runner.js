0ac2ca074a8c3823dafd0620c2e5c676a0979c3b
Livingston Samuel <livingston.samuel@gmail.com>
2014-04-18 22:10:50 +0530

    configuration changes for Sauce to show the tests in dashboard

--- test/runner.js
+++ test/runner.js
@@ -3,17 +3,16 @@ var MochaSauce = require("../node_modules/mocha-sauce/index.js");
 // configure
 var sauce = new MochaSauce({
     name: "SessionStore",
+    username: 'sessionstore',
     host: "localhost",
     port: 4445,
     url: "http://localhost:8080/test",
     build: process.env.TRAVIS_JOB_ID
 });
 
-
 // setup what browsers to test with
 sauce.browser({ browserName: "chrome", platform: "Windows 7" });
 sauce.browser({ browserName: "firefox", platform: "Windows 7" });
-
 
 sauce.on('init', function(browser) {
   console.log('  init : %s %s', browser.browserName, browser.platform);
