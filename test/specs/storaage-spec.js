6793b2710ae8f33828ba62edacfc3c33d6ddc838
Livingston Samuel <livingston.samuel@gmail.com>
2014-04-21 01:27:59 +0530

    added localStorage tests & grouped specs

--- test/specs/storaage-spec.js
+++ test/specs/storaage-spec.js
@@ -1,6 +1,6 @@
 describe('Storaage', function () {
 
-  describe('sessionStorage', function () {
+  describe('common functionality', function () {
     beforeEach(function (done) {
       window.sessionStorage.clear();
       done();
@@ -21,6 +21,26 @@ describe('Storaage', function () {
       expect(store.ns).to.equal('sessions');
     });
 
+    it('retrives the individual property from data', function () {
+      var store = new Storaage('testing', { storage: ['session', 'local'] });
+
+      expect(store.getItem('storage')).to.eql(['session', 'local']);
+    });
+
+    it('sets the individual property to data', function () {
+      var store = new Storaage('testing', { storage: ['session', 'local'] });
+
+      store.setItem('storage', 'IndexedDB');
+
+      expect(store.getItem('storage')).to.eql('IndexedDB');
+    });
+  });
+
+  describe('sessionStorage', function () {
+    beforeEach(function (done) {
+      window.sessionStorage.clear();
+      done();
+    });
 
     it('to set data property', function () {
       var store = new Storaage('sessionstore', { version: '1.0.0' });
@@ -29,7 +49,7 @@ describe('Storaage', function () {
       expect(store.data).to.eql({ version: '1.0.0' })
     });
 
-    it('to select the right storage', function () {
+    it('to set storage as sessionStorage', function () {
       var store = new Storaage('sessionstore', {}, false);
 
       expect(store.storage).to.equal(window.sessionStorage);
@@ -65,20 +85,6 @@ describe('Storaage', function () {
       expect(JSON.parse(sessionStorage.getItem('sessionstore'))).to.eql({ test: 123 });
     });
 
-    it('retrives the individual property from data', function () {
-      var store = new Storaage('testing', { storage: ['session', 'local'] });
-
-      expect(store.getItem('storage')).to.eql(['session', 'local']);
-    });
-
-    it('sets the individual property to data', function () {
-      var store = new Storaage('testing', { storage: ['session', 'local'] });
-
-      store.setItem('storage', 'IndexedDB');
-
-      expect(store.getItem('storage')).to.eql('IndexedDB');
-    });
-
     it('data should be identical among instances using the same namespace', function () {
       var store1 =  new Storaage('sessionstore', { version: '1.0.0' });
       var store2 =  new Storaage('sessionstore', { version: '2.0.0' });
@@ -91,6 +97,82 @@ describe('Storaage', function () {
       var store2 =  new Storaage('sessionstore2', { version: '2.0.0' });
 
       expect(store1.data).to.not.eql(store2.data);
+    });
+
+    it('to not use sessionStorage when persist is true', function () {
+      var store = new Storaage('sessionstore', {}, true);
+
+      expect(store.storage).to.not.equal(window.sessionStorage);
+    });
+  });
+
+  describe('localStorage', function () {
+    beforeEach(function (done) {
+      window.localStorage.clear();
+      done();
+    });
+
+    it('to set data property', function () {
+      var store = new Storaage('localstore', { version: '1.0.0' }, true);
+
+      expect(store.data).to.be.a('object');
+      expect(store.data).to.eql({ version: '1.0.0' })
+    });
+
+    it('to set storage as localStorage', function () {
+      var store = new Storaage('localstore', {}, true);
+
+      expect(store.storage).to.not.equal(window.sessionStorage);
+      expect(store.storage).to.equal(window.localStorage);
+    });
+
+    it('to not modify the storage property', function () {
+      var store = new Storaage('localstore', {}, true);
+
+      store.storage = {};
+
+      expect(store.storage).to.equal(window.localStorage);
+    });
+
+    it('to store data passed in as argument, in localStorage', function () {
+      var store = new Storaage('localstore', { version: '2.0.0' }, true);
+
+      expect(JSON.parse(localStorage.getItem('localstore'))).to.eql({ version: '2.0.0' });
+    });
+
+    it('to store data set as value in localStorage', function () {
+      var store = new Storaage('localstore', null, true);
+
+      store.data = { test: '1234' };
+
+      expect(JSON.parse(localStorage.getItem('localstore'))).to.eql({ test: '1234' });
+    });
+
+    it('to not overwrite data in localStorage', function () {
+      localStorage.setItem('localstore', '{ "test": 123 }');
+      var store = new Storaage('localstore', null, true);
+
+      expect(JSON.parse(localStorage.getItem('localstore'))).to.eql(store.data);
+    });
+
+    it('data should be identical among instances using the same namespace', function () {
+      var store1 =  new Storaage('localstore', { version: '1.0.0' }, true);
+      var store2 =  new Storaage('localstore', { version: '2.0.0' }, true);
+
+      expect(store1.data).to.eql(store2.data);
+    })
+
+    it('data should be unique among instances with different namespace', function () {
+      var store1 =  new Storaage('localstore', { version: '1.0.0' }, true);
+      var store2 =  new Storaage('localstore2', { version: '2.0.0' }, true);
+
+      expect(store1.data).to.not.eql(store2.data);
+    });
+
+    it('to not use localStorage when persist is false', function () {
+      var store = new Storaage('localstore', {}, true);
+
+      expect(store.storage).to.not.equal(window.sessionStorage);
     });
   });
 
