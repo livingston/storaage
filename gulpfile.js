93e0e64d98624c2cf548835c003b5a65b032eaf8
Livingston Samuel <livingston.samuel@gmail.com>
2014-07-31 14:31:45 +0100

    Switching to Snap-ci from SauceLabs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp    = require("gulp");
-var connect = require('connect');
-var http    = require('http');
+var gulp           = require("gulp");
+var connect        = require('connect');
+var http           = require('http');
+
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
 
 gulp.task('serve', function(){
   var buildPath = __dirname;
@@ -11,4 +13,14 @@ gulp.task('serve', function(){
   http.createServer(app).listen(4000);
 });
 
+gulp.task('test', function (){
+  var stream = mochaPhantomJS();
+
+  stream.write({path: 'test/runner.html'});
+  stream.end();
+
+  return stream;
+});
+
 gulp.task('default', ['serve']);
+gulp.task('specs', ['serve', 'test']);
